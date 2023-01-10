ba2e1f579406a9a4d1a077a0d301e688d9cf3e10
Duong Quoc Khanh <dqkqdlot@gmail.com>
2023-01-11 02:00:20 +0900

    Add expression for binary grammar.

---
+++ src/expr.rs
@@ -0,0 +1,62 @@
+use crate::token::{Number, Token};
+
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) enum Expr {
+    Binary(Binary),
+    Unary(Unary),
+    Literal(Literal),
+    Grouping(Grouping),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct Binary {
+    left: Box<Expr>,
+    right: Box<Expr>,
+    operator: Token,
+}
+
+impl Binary {
+    pub fn new(left: Expr, operator: Token, right: Expr) -> Self {
+        Binary {
+            left: Box::new(left),
+            right: Box::new(right),
+            operator,
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct Unary {
+    operator: Token,
+    right: Box<Expr>,
+}
+
+impl Unary {
+    pub fn new(operator: Token, right: Expr) -> Self {
+        Unary {
+            operator,
+            right: Box::new(right),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) enum Literal {
+    Null,
+    Number(Number),
+    String(String),
+    Bool(bool),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub(crate) struct Grouping {
+    expr: Box<Expr>,
+}
+
+impl Grouping {
+    pub fn new(expr: Expr) -> Self {
+        Grouping {
+            expr: Box::new(expr),
+        }
+    }
+}
