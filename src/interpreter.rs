bad58a8803727942efb4aba84fcd0aa38a1f26da
Duong Quoc Khanh <dqkqdlot@gmail.com>
2023-01-15 16:38:08 +0900

    Add test for statements, separate test for expression.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -158,145 +158,232 @@ mod test {
 
     use super::*;
 
-    fn interpret(source: &str) -> InterpreterResult<Object> {
-        let mut scanner = Scanner::new(source);
-        scanner.scan_tokens();
-        assert!(!scanner.had_error());
-        let mut parser = Parser::from(&scanner);
-        let expr = parser.expression().unwrap();
-        let mut interpreter = Interpreter::default();
-        interpreter.expr(&expr)
-    }
+    mod test_expr {
+        use super::*;
+        fn interpret(source: &str) -> InterpreterResult<Object> {
+            let mut scanner = Scanner::new(source);
+            scanner.scan_tokens();
+            assert!(!scanner.had_error());
+            let mut parser = Parser::from(&scanner);
+            let expr = parser.expression().unwrap();
+            let mut interpreter = Interpreter::default();
+            interpreter.expr(&expr)
+        }
 
-    fn test_interpreter(sources: &[&str], expected_results: &[InterpreterResult<Object>]) {
-        for (&src, expected) in std::iter::zip(sources, expected_results) {
-            let result = interpret(src);
-            assert_eq!(&result, expected);
+        fn test_interpreter(sources: &[&str], expected_results: &[InterpreterResult<Object>]) {
+            for (&src, expected) in std::iter::zip(sources, expected_results) {
+                let result = interpret(src);
+                assert_eq!(&result, expected);
+            }
+        }
+
+        #[test]
+        fn expr_only_number_could_be_unary() {
+            let sources = ["-1", "-nil", "-true", "-false", "-\"a\""];
+            let expected_results = [
+                Ok(Object::Number(-1.0)),
+                Err(RuntimeError::from((1, ObjectError::negative()))),
+                Err(RuntimeError::from((1, ObjectError::negative()))),
+                Err(RuntimeError::from((1, ObjectError::negative()))),
+                Err(RuntimeError::from((1, ObjectError::negative()))),
+            ];
+            test_interpreter(&sources, &expected_results);
+        }
+
+        #[test]
+        fn expr_nil_and_false_are_false() {
+            let sources = ["!1", "!nil", "!true", "!false", "!\"a\""];
+            let expected_results = [
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(true)),
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(true)),
+                Ok(Object::Bool(false)),
+            ];
+            test_interpreter(&sources, &expected_results);
+        }
+
+        #[test]
+        fn expr_binary_subtract() {
+            let sources = ["2-3", "\"a\" - true"];
+            let expected_results = [
+                Ok(Object::Number(-1.0)),
+                Err(RuntimeError::from((1, ObjectError::subtract()))),
+            ];
+            test_interpreter(&sources, &expected_results);
+        }
+
+        #[test]
+        fn expr_binary_multiplication() {
+            let sources = ["2 * 3", "nil * nil"];
+            let expected_results = [
+                Ok(Object::Number(6.0)),
+                Err(RuntimeError::from((1, ObjectError::multiplication()))),
+            ];
+            test_interpreter(&sources, &expected_results);
+        }
+
+        #[test]
+        fn expr_binary_division() {
+            let sources = ["3 / 2", "nil / nil"];
+            let expected_results = [
+                Ok(Object::Number(1.5)),
+                Err(RuntimeError::from((1, ObjectError::division()))),
+            ];
+            test_interpreter(&sources, &expected_results);
+        }
+
+        #[test]
+        fn expr_binary_add() {
+            let sources = ["3 + 2", "\"Hello\" + \" World\"", "true + 1", "nil + 2"];
+            let expected_results = [
+                Ok(Object::Number(5.0)),
+                Ok(Object::String("Hello World".to_string())),
+                Err(RuntimeError::from((1, ObjectError::addition()))),
+                Err(RuntimeError::from((1, ObjectError::addition()))),
+            ];
+            test_interpreter(&sources, &expected_results);
+        }
+
+        #[test]
+        fn expr_binary_comparision() {
+            let sources = [
+                "1 > 2",
+                "1 >= 2",
+                "1 < 2",
+                "1 <= 2",
+                "true > false",
+                "\"a\" >= \"b\"",
+                "nil < nil",
+                "nil <= true",
+                "true == true",
+                "true == false",
+                "true != true",
+                "true != false",
+                "nil == nil",
+                "nil == 1",
+                "nil != nil",
+                "nil != 1",
+                "1 == 2",
+                "1 == 1",
+                "1 != 2",
+                "1 != 1",
+                "\"hello\" == \"hello\"",
+                "\"hello\" == \"world\"",
+                "\"hello\" != \"hello\"",
+                "\"hello\" != \"world\"",
+            ];
+            let expected_results = [
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(true)),
+                Ok(Object::Bool(true)),
+                Err(RuntimeError::from((1, ObjectError::comparision()))),
+                Err(RuntimeError::from((1, ObjectError::comparision()))),
+                Err(RuntimeError::from((1, ObjectError::comparision()))),
+                Err(RuntimeError::from((1, ObjectError::comparision()))),
+                Ok(Object::Bool(true)),
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(true)),
+                Ok(Object::Bool(true)),
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(true)),
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(true)),
+                Ok(Object::Bool(true)),
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(true)),
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(false)),
+                Ok(Object::Bool(true)),
+            ];
+            test_interpreter(&sources, &expected_results);
         }
     }
 
-    #[test]
-    fn expr_only_number_could_be_unary() {
-        let sources = ["-1", "-nil", "-true", "-false", "-\"a\""];
-        let expected_results = [
-            Ok(Object::Number(-1.0)),
-            Err(RuntimeError::from((1, ObjectError::negative()))),
-            Err(RuntimeError::from((1, ObjectError::negative()))),
-            Err(RuntimeError::from((1, ObjectError::negative()))),
-            Err(RuntimeError::from((1, ObjectError::negative()))),
-        ];
-        test_interpreter(&sources, &expected_results);
-    }
+    mod test_stmt {
 
-    #[test]
-    fn expr_nil_and_false_are_false() {
-        let sources = ["!1", "!nil", "!true", "!false", "!\"a\""];
-        let expected_results = [
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(true)),
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(true)),
-            Ok(Object::Bool(false)),
-        ];
-        test_interpreter(&sources, &expected_results);
-    }
+        use std::io::Write;
 
-    #[test]
-    fn expr_binary_subtract() {
-        let sources = ["2-3", "\"a\" - true"];
-        let expected_results = [
-            Ok(Object::Number(-1.0)),
-            Err(RuntimeError::from((1, ObjectError::subtract()))),
-        ];
-        test_interpreter(&sources, &expected_results);
-    }
+        use super::*;
 
-    #[test]
-    fn expr_binary_multiplication() {
-        let sources = ["2 * 3", "nil * nil"];
-        let expected_results = [
-            Ok(Object::Number(6.0)),
-            Err(RuntimeError::from((1, ObjectError::multiplication()))),
-        ];
-        test_interpreter(&sources, &expected_results);
-    }
+        fn test_parser(source: &str, expected_output: &str) -> Result<(), std::io::Error> {
+            let mut result = Vec::new();
+            let mut scanner = Scanner::new(source);
+            scanner.scan_tokens();
+            for error in scanner.errors() {
+                writeln!(&mut result, "{:?}", error)?;
+            }
 
-    #[test]
-    fn expr_binary_division() {
-        let sources = ["3 / 2", "nil / nil"];
-        let expected_results = [
-            Ok(Object::Number(1.5)),
-            Err(RuntimeError::from((1, ObjectError::division()))),
-        ];
-        test_interpreter(&sources, &expected_results);
-    }
+            let mut parser = Parser::from(&scanner);
+            let statements = parser.parse();
+            for error in parser.errors() {
+                writeln!(&mut result, "{:?}", error)?;
+            }
 
-    #[test]
-    fn expr_binary_add() {
-        let sources = ["3 + 2", "\"Hello\" + \" World\"", "true + 1", "nil + 2"];
-        let expected_results = [
-            Ok(Object::Number(5.0)),
-            Ok(Object::String("Hello World".to_string())),
-            Err(RuntimeError::from((1, ObjectError::addition()))),
-            Err(RuntimeError::from((1, ObjectError::addition()))),
-        ];
-        test_interpreter(&sources, &expected_results);
-    }
+            let mut interpreter = Interpreter::new(&mut result);
+            if let Err(error) = interpreter.interpret(&statements) {
+                interpreter.write(&error.to_string())?;
+            }
 
-    #[test]
-    fn expr_binary_comparision() {
-        let sources = [
-            "1 > 2",
-            "1 >= 2",
-            "1 < 2",
-            "1 <= 2",
-            "true > false",
-            "\"a\" >= \"b\"",
-            "nil < nil",
-            "nil <= true",
-            "true == true",
-            "true == false",
-            "true != true",
-            "true != false",
-            "nil == nil",
-            "nil == 1",
-            "nil != nil",
-            "nil != 1",
-            "1 == 2",
-            "1 == 1",
-            "1 != 2",
-            "1 != 1",
-            "\"hello\" == \"hello\"",
-            "\"hello\" == \"world\"",
-            "\"hello\" != \"hello\"",
-            "\"hello\" != \"world\"",
-        ];
-        let expected_results = [
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(true)),
-            Ok(Object::Bool(true)),
-            Err(RuntimeError::from((1, ObjectError::comparision()))),
-            Err(RuntimeError::from((1, ObjectError::comparision()))),
-            Err(RuntimeError::from((1, ObjectError::comparision()))),
-            Err(RuntimeError::from((1, ObjectError::comparision()))),
-            Ok(Object::Bool(true)),
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(true)),
-            Ok(Object::Bool(true)),
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(true)),
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(true)),
-            Ok(Object::Bool(true)),
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(true)),
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(false)),
-            Ok(Object::Bool(true)),
-        ];
-        test_interpreter(&sources, &expected_results);
+            let result = String::from_utf8(result).unwrap();
+            assert_eq!(result.trim(), expected_output.trim());
+
+            Ok(())
+        }
+
+        #[test]
+        fn variable_declaration() -> Result<(), std::io::Error> {
+            let source = "
+var x = 1;
+print x;
+var y
+";
+            let expected_output = "
+[line 5]: ParseError: Expected `;`. Found `EOF`
+1";
+            test_parser(source, expected_output)?;
+            Ok(())
+        }
+
+        #[test]
+        fn assignment() -> Result<(), std::io::Error> {
+            let source = "
+var x = 1;
+x = 2;
+print x;
+x = y;
+";
+            let expected_output = "
+2
+[line 5]: RuntimeError: Undefined variable `y`";
+            test_parser(source, expected_output)?;
+            Ok(())
+        }
+
+        #[test]
+        fn block() -> Result<(), std::io::Error> {
+            let source = "
+var x = 1;
+var y = 2;
+{
+var x = 3;
+print x;
+print y;
+}
+print x;
+print y;
+";
+            let expected_output = "
+3
+2
+1
+2
+";
+            test_parser(source, expected_output)?;
+            Ok(())
+        }
     }
 }
